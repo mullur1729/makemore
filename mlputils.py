4b83eb7f6a1f978da5be36b25aac6f54bffff870
mullur1729 <mullur1729@gmail.com>
2025-12-05 20:41:01 -0600

    Implemented hierarchical wavenet model

--- mlputils.py
+++ mlputils.py
@@ -3,8 +3,7 @@ import torch
 
 class Linear:
     def __init__(self, fan_in, fan_out, bias=True):
-        g = torch.Generator().manual_seed(2147483647)
-        self.weight = torch.randn((fan_in, fan_out), generator = g) / fan_in**0.5 # kaiming initialization
+        self.weight = torch.randn((fan_in, fan_out)) / fan_in**0.5 # kaiming initialization
         self.bias = torch.zeros(fan_out) if bias else None
 
     def __call__(self, x):
@@ -34,8 +33,12 @@ class BatchNorm1D:
     def __call__(self, x):
 
         if self.training:
-            xmean = x.mean(0, keepdim=True)
-            xvar = x.var(0, keepdim=True)
+            if x.ndim == 2:
+                dim = 0
+            elif x.ndim == 3:
+                dim = (0,1)
+            xmean = x.mean(dim, keepdim=True)
+            xvar = x.var(dim, keepdim=True)
         else:
             xmean = self.running_mean
             xvar = self.running_var
@@ -60,4 +63,44 @@ class Tanh:
         return self.out
     def parameters(self):
         return []
+    
+class Embedding:
+    def __init__(self, num_embeddings, embedding_dim):
+        self.weight = torch.randn((num_embeddings, embedding_dim)) / (embedding_dim ** 0.5)
+
+    def __call__(self, IX):
+        self.out = self.weight[IX]
+        return self.out
+    
+    def parameters(self):
+        return [self.weight]
+    
+class FlattenConsecutive:
+    def __init__(self, n):
+        self.n = n
+    def __call__(self, x):
+        B, T, C = x.shape
+        x = x.view(B, T//self.n, C * self.n)
+
+        if x.shape[1] == 1:
+            x = x.squeeze(1)
+        self.out = x
+
+        return self.out
+    
+    def parameters(self):
+        return []
+    
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
+        
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
     
